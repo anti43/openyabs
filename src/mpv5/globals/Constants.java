7bccd50ff909ac858b58002528129bd7469d0068
senior.weber <senior.weber@c7c5359e-154f-0410-9d44-fb436a9bad0b>
2009-12-25 17:29:07 +0000

    Yabs 1.0 Final - Splash image

--- src/mpv5/globals/Constants.java
+++ src/mpv5/globals/Constants.java
@@ -46,7 +46,7 @@ public interface Constants {
      *
      * Alias
      */
-    public static final String TITLE = "[Róisín Dubh]";
+    public static final String TITLE = "[Sláinte!]";
     /**
      *
      * Name of the main JAR
@@ -81,7 +81,7 @@ public interface Constants {
     public static String XML_ROOT = "mpv5";
     public static String XML_DOCTYPE_ID = "-//http://openyabs.org//DTD mpv5 V 1.0//EN";
     public static String XML_DOCTYPE_URL = "http://openyabs.org/files/yabs_import_1.0.dtd";
-    public static String SPLASH_IMAGE = "/mpv5/resources/images/background_b.png";
+    public static String SPLASH_IMAGE = "/mpv5/resources/images/background_c.png";
     public static String ABOUT_IMAGE = "/mpv5/resources/images/about1.png";
     public static String WEBSITE = "http://openyabs.org";
     public static final String[] CONTRIBUTORS = new String[]{
