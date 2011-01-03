86ee0f53a35ec23c7ce5d332931211d64f7b8088
senior.weber <senior.weber@c7c5359e-154f-0410-9d44-fb436a9bad0b>
2011-01-03 19:27:12 +0000

    1.175.3 release

--- src/mpv5/globals/Constants.java
+++ src/mpv5/globals/Constants.java
@@ -28,7 +28,7 @@ public interface Constants {
      * 
      * Full Version number
      */
-    public static final String VERSION = "Yabs-1.175.2";
+    public static final String VERSION = "Yabs-1.175.3";
 
     /**
      * The original database version of this release
