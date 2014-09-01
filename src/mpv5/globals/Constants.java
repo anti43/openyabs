3dd1406aeae0c863e23e5251efe9b34296cfa3b7
Andreas Weber <niemandr@posteo.de>
2014-09-01 06:59:33 +0200

    Version++ due to code refacturing
    and upcoming changes in template handling and views. a new start

--- src/mpv5/globals/Constants.java
+++ src/mpv5/globals/Constants.java
@@ -30,7 +30,7 @@ public interface Constants {
      * 
      * Full Version number
      */
-    public static final String VERSION = "Yabs-1.2";
+    public static final String VERSION = "Yabs-1.2.5-alpha1";
 
     /**
      * The original database version of this release
@@ -57,7 +57,7 @@ public interface Constants {
      *
      * Alias
      */
-    public static final String TITLE = "Yabs - Marla2";
+    public static final String TITLE = "Yabs - Appelpie";
     /**
      *
      * Name of the main JAR
